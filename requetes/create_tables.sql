7933d3c251646e02fb256fb151f75f59ccfd2712
morganb <morgan.bleunven@maif.fr>
2024-03-12 14:12:38 +0100

    creation des tables et MPD

--- requetes/create_tables.sql
+++ requetes/create_tables.sql
@@ -1,102 +1,67 @@
 /*==== DROP TOTALE SI NECESSAIRE ====*/
 /*
-DROP TABLE comment,publication,role_dbuser,dbuser,role; 
+ DROP TABLE comment_user_like,
+ publi_follower,publi_liker,role_user,comment,
+ publication,user,role; 
 */
 /*=============== TABLE USER ==============*/
-CREATE TABLE dbuser(
+CREATE TABLE "user"(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255),
-    role_id INT NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
 /*=============== TABLE ROLE ==============*/
 CREATE TABLE role(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255) NOT NULL
 );
 
-/*======= TABLE DE JOINTURE ROLE_DBUSER ===*/
-CREATE TABLE role_dbuser(
+/*======= TABLE DE JOINTURE ROLE_USER ===*/
+CREATE TABLE role_user(
     role_id INT NOT NULL,
-    dbuser_id INT NOT NULL,
+    user_id INT NOT NULL,
     CONSTRAINT fk_role
         FOREIGN KEY(role_id)
         REFERENCES role(id)
         ON DELETE CASCADE,
-    CONSTRAINT fk_dbuser
-        FOREIGN KEY (dbuser_id)
-        REFERENCES dbuser(id)
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES "user"(id)
         ON DELETE CASCADE
 );
 
 /*======= TABLE PUBLICATION  ===*/
 CREATE TABLE publication(
     id SERIAL PRIMARY KEY,
-    content VARCHAR(255),
+    content VARCHAR(255) NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    owner_id INT NOT NULL,
-    liker_id INT,
-    follower_id INT,
-    CONSTRAINT fk_owner
-        FOREIGN KEY (owner_id)
-        REFERENCES dbuser(id)
-        ON DELETE SET NULL
-);
-
-/*======= TABLE DE JOINTURE publi_user_follow ===*/
-CREATE TABLE publi_follower(
-    follower_id INT,
-    user_id INT,
-    CONSTRAINT fk_publi_follower
-        FOREIGN KEY (follower_id)
-        REFERENCES publication(follower_id),
-    CONSTRAINT fk_user_follower
-        FOREIGN KEY(user_id)
-        REFERENCES dbuser(id)
+    owner_id INT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+    publication_id INT REFERENCES publication(id) ON DELETE SET NULL
 );
 
 /*======= TABLE DE JOINTURE publi_user_like ====*/
-CREATE TABLE publi_user(
-    liker_id INT,
+CREATE TABLE publi_user_like(
     user_id INT,
-    CONSTRAINT fk_publi_liker
-        FOREIGN KEY (liker_id)
-        REFERENCES publication(liker_id),
-    CONSTRAINT fk_user_liker
+    liked_id INT,
+    CONSTRAINT fk_user
         FOREIGN KEY (user_id)
-        REFERENCES dbuser(id)
-);
-
-/*======= TABLE COMMENT  ===*/
-CREATE TABLE comment(
-    id SERIAL PRIMARY KEY,
-    text VARCHAR(255),
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    publication_id INT NOT NULL,
-    owner_id INT NOT NULL,
-    liker_id INT,
-    CONSTRAINT fk_publication
-        FOREIGN KEY (publication_id)
+        REFERENCES "user"(id),
+    CONSTRAINT fk_liked
+        FOREIGN KEY (liked_id)
         REFERENCES publication(id)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_owner_id
-        FOREIGN KEY (owner_id)
-        REFERENCES dbuser(id)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_liker_id
-        FOREIGN KEY (liker_id)
-        REFERENCES dbuser(id)
-        ON DELETE SET NULL
 );
 
-/*======= TABLE DE JOINTURE comment_user_owner ===*/
-CREATE TABLE comment_user_owner(
-    comment_id INT NOT NULL,
-    dbuser_id INT NOT NULL,
-    CONSTRAINT fk_user_owner
-        
+/*======= TABLE DE JOINTURE user_follow ===*/
+CREATE TABLE user_follow(
+    user_id INT,
+    followed_id INT,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES "user"(id),
+    CONSTRAINT fk_followed
+        FOREIGN KEY (followed_id)
+        REFERENCES "user"(id)
 );
-/*======= TABLE DE JOINTURE comment_user_like ===*/
